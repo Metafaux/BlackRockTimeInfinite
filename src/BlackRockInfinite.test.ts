b9df87e3534672bb6064329682ca40c1b2ae6f3d
Alex W <alex@metafaux.com>
2021-07-15 18:24:01 -0700

    Typescript functions and unit tests
    getBurnTime, getBurnYear, isItBurnNight,
    getNextBurnTime, getMetricTime

--- src/BlackRockInfinite.test.ts
+++ src/BlackRockInfinite.test.ts
@@ -1,17 +1,153 @@
-import { getLaborDate, SEPTEMBER_INDEX } from './BlackRockInfinite';
+import {
+  getLaborDate,
+  getBurnTime,
+  addLeadingZeroes,
+  getFormattedDateString,
+  getNextBurnTime,
+  getMetricTime,
+  isItBurnNight,
+} from './BlackRockInfinite';
 
 describe('Labor Day date', () => {
   it('calculates 2021 Labor Day to be Sep. 6', () => {
     const laborDay2021 = getLaborDate(2021);
     expect(laborDay2021.getUTCDate()).toEqual(6);
-    expect(laborDay2021.getUTCMonth()).toEqual(SEPTEMBER_INDEX);
+    expect(laborDay2021.getUTCMonth()).toEqual(8);
   });
   it('calculates 2023 Labor Day to be Sep. 4', () => {
     const laborDay2023 = getLaborDate(2023);
     expect(laborDay2023.getUTCDate()).toEqual(4);
   });
+  it('calculates 2024 Labor Day to be Sep. 2', () => {
+    const laborDay2024 = getLaborDate(2024);
+    expect(laborDay2024.getUTCDate()).toEqual(2);
+  });
   it('calculates 2031 Labor Day to be Sep. 1', () => {
     const laborDay2031 = getLaborDate(2031);
     expect(laborDay2031.getUTCDate()).toEqual(1);
+  });
+  it('calculates 2032 Labor Day to be Sep. 6', () => {
+    const laborDay2032 = getLaborDate(2032);
+    expect(laborDay2032.getUTCDate()).toEqual(6);
+  });
+  it('calculates 2033 Labor Day to be Sep. 5', () => {
+    const laborDay2033 = getLaborDate(2033);
+    expect(laborDay2033.getUTCDate()).toEqual(5);
+  });
+});
+
+describe('Burn time', () => {
+  const pacificTimeZone = 'America/Los_Angeles';
+  it('calculates 2021 Burn to happen Sep. 4, 09:23:23 PM PDT', () => {
+    const burnTime = getBurnTime(2021);
+    const formattedPacificDate = getFormattedDateString(
+      burnTime,
+      true,
+      pacificTimeZone
+    );
+    expect(formattedPacificDate).toEqual('Sep 4, 2021, 09:23:23 PM PDT');
+  });
+  it('calculates 2024 Burn to happen Aug 31, 09:23:23 PM PDT', () => {
+    const burnTime = getBurnTime(2024);
+    const formattedPacificDate = getFormattedDateString(
+      burnTime,
+      true,
+      pacificTimeZone
+    );
+    expect(formattedPacificDate).toEqual('Aug 31, 2024, 09:23:23 PM PDT');
+  });
+  it('calculates 2031 Burn to happen Aug 30, 09:23:23 PM PDT', () => {
+    const burnTime = getBurnTime(2031);
+    const formattedPacificDate = getFormattedDateString(
+      burnTime,
+      true,
+      pacificTimeZone
+    );
+    expect(formattedPacificDate).toEqual('Aug 30, 2031, 09:23:23 PM PDT');
+  });
+  it('calculates the next burn after Sep 1, 2023', () => {
+    const sep2date = Date.UTC(2023, 8, 1);
+    const nextBurn = getNextBurnTime(sep2date);
+    const formattedPacificDate = getFormattedDateString(
+      nextBurn,
+      true,
+      pacificTimeZone
+    );
+    expect(formattedPacificDate).toEqual('Sep 2, 2023, 09:23:23 PM PDT');
+  });
+  it('calculates the next burn after Sep 2, 2023, 9:23:24 PM PDT', () => {
+    const sep2date = Date.UTC(2023, 8, 3, 4, 23, 24);
+    const nextBurn = getNextBurnTime(sep2date);
+    const formattedPacificDate = getFormattedDateString(
+      nextBurn,
+      true,
+      pacificTimeZone
+    );
+    expect(formattedPacificDate).toEqual('Aug 31, 2024, 09:23:23 PM PDT');
+  });
+  it('shows the time left from 9:23:23 AM PDT July 16, 2021 to be 050:05:00:00', () => {
+    const july16date = Date.UTC(2021, 6, 16, 16, 23, 23);
+    const burnDate = getNextBurnTime(july16date);
+    const metricTimeObj = getMetricTime(burnDate.getTime(), july16date);
+    expect(metricTimeObj.year).toEqual('36');
+    expect(metricTimeObj.days).toEqual('050');
+    expect(metricTimeObj.hours).toEqual('05');
+    expect(metricTimeObj.minutes).toEqual('00');
+    expect(metricTimeObj.seconds).toEqual('00');
+  });
+  it('shows that Sep. 4, 2021, 21:23:22 PDT is NOT during Burn Night', () => {
+    const sep4night = Date.UTC(2021, 8, 5, 4, 23, 22);
+    const isBurnNight = isItBurnNight(sep4night);
+    const formattedPacificDate = getFormattedDateString(
+      new Date(sep4night),
+      true,
+      pacificTimeZone
+    );
+    expect(isBurnNight).toEqual(false);
+    expect(formattedPacificDate).toEqual('Sep 4, 2021, 09:23:22 PM PDT');
+  });
+  it('shows that Sep. 4, 2021, 21:23:24 PDT is during Burn Night', () => {
+    const sep4night = Date.UTC(2021, 8, 5, 4, 23, 24);
+    const isBurnNight = isItBurnNight(sep4night);
+    const formattedPacificDate = getFormattedDateString(
+      new Date(sep4night),
+      true,
+      pacificTimeZone
+    );
+    expect(isBurnNight).toEqual(true);
+    expect(formattedPacificDate).toEqual('Sep 4, 2021, 09:23:24 PM PDT');
+  });
+  it('shows that Sep. 5, 2021, 06:29:22 PDT is during Burn Night', () => {
+    const sep4night = Date.UTC(2021, 8, 5, 13, 29, 22);
+    const isBurnNight = isItBurnNight(sep4night);
+    const formattedPacificDate = getFormattedDateString(
+      new Date(sep4night),
+      true,
+      pacificTimeZone
+    );
+    expect(isBurnNight).toEqual(true);
+    expect(formattedPacificDate).toEqual('Sep 5, 2021, 06:29:22 AM PDT');
+  });
+  it('shows that Sep. 5, 2021, 06:29:24 PDT is NOT during Burn Night', () => {
+    const sep4night = Date.UTC(2021, 8, 5, 13, 29, 24);
+    const isBurnNight = isItBurnNight(sep4night);
+    const formattedPacificDate = getFormattedDateString(
+      new Date(sep4night),
+      true,
+      pacificTimeZone
+    );
+    expect(isBurnNight).toEqual(false);
+    expect(formattedPacificDate).toEqual('Sep 5, 2021, 06:29:24 AM PDT');
+  });
+});
+
+describe('date + time string formatting', () => {
+  it('adds five leading zeroes to number 3', () => {
+    const formattedNumber = addLeadingZeroes('3', 6);
+    expect(formattedNumber).toEqual('000003');
+  });
+  it('adds no leading zeroes to number 1074', () => {
+    const formattedNumber = addLeadingZeroes('1074', 4);
+    expect(formattedNumber).toEqual('1074');
   });
 });
--- src/BlackRockInfinite.ts
+++ src/BlackRockInfinite.ts
@@ -1,8 +1,28 @@
-const BURN_HOUR_UTC = 4; // 21:00 PDT
-const MONDAY_INDEX = 1;
-const BURN_DAY_UTC = 0; // Sunday 04:00 UTC / Saturday 21:00 PDT
-const BURN_MINUTE = 23;
-export const SEPTEMBER_INDEX = 8;
+/**
+ * Black Rock Infinite: TypeScript
+ */
+
+const METRIC_SECOND_VALUE_MS = 864;
+enum timerDisplayElement {
+  DAYS = 'days',
+  HOURS = 'hours',
+  MINUTES = 'minutes',
+  SECONDS = 'seconds',
+}
+
+/**
+ *
+ * @param numberString
+ * @param digitCount
+ * @returns
+ */
+export const addLeadingZeroes = (numberString: string, digitCount: number) => {
+  let resultString = numberString;
+  while (resultString.length < digitCount) {
+    resultString = '0' + resultString;
+  }
+  return resultString;
+};
 
 /**
  * Finds the first Monday of September
@@ -10,8 +30,13 @@ export const SEPTEMBER_INDEX = 8;
  * @param year number: full year
  */
 export const getLaborDate = (year: number) => {
+  const SEPTEMBER_INDEX = 8;
+  const MONDAY_INDEX = 1;
+
+  // Start with 1st of September
   let laborDate = new Date(Date.UTC(year, SEPTEMBER_INDEX, 1));
 
+  // Check until first Monday is found
   while (laborDate.getUTCDay() !== MONDAY_INDEX) {
     laborDate.setUTCDate(laborDate.getUTCDate() + 1);
   }
@@ -20,20 +45,91 @@ export const getLaborDate = (year: number) => {
 };
 
 export const getFormattedDateString = (
-  dateValue: number,
+  date: Date,
   showLocalTimeZone?: boolean,
-  locale?: string,
-  timeZone?: string
+  timeZone?: string,
+  locale?: string
 ) => {
-  const date = new Date(dateValue);
-  const dateLocale = locale || navigator.language;
+  // const date = new Date(dateValue);
+  const dateLocale = locale || 'en-US';
   return date.toLocaleString(dateLocale, {
     hour: '2-digit',
     minute: '2-digit',
+    second: '2-digit',
     month: 'short',
     day: 'numeric',
     year: 'numeric',
     timeZone: timeZone,
     timeZoneName: showLocalTimeZone ? 'short' : undefined,
   });
+};
+
+export const getBurnTime = (year: number) => {
+  const BURN_HOUR_UTC = 4; // 04:00 UTC = 21:00 PDT
+  const BURN_MINUTE = 23; // Burns at 21:23:23 PDT
+
+  let burnDate = getLaborDate(year);
+  burnDate.setUTCDate(burnDate.getUTCDate() - 1);
+  burnDate.setUTCHours(BURN_HOUR_UTC);
+  burnDate.setUTCMinutes(BURN_MINUTE);
+  burnDate.setUTCSeconds(BURN_MINUTE);
+
+  // maybe this should just return getTime() number
+  return burnDate;
+};
+
+const getBurnYear = (burnTime: number) => {
+  const burnYear = new Date(burnTime).getFullYear();
+  const BURN_YEAR_ZERO = 1985;
+  return burnYear - BURN_YEAR_ZERO;
+};
+
+// length of early September night in Gerlach, in MS.
+const NINE_HR_SIX_M_MS = 9 * 60 * 60 * 1000 + 6 * 60 * 1000;
+
+export const isItBurnNight = (time: number) => {
+  const timeDate = new Date(time);
+  const calendarYearBurn = getBurnTime(timeDate.getFullYear());
+  const burnTime = calendarYearBurn.getTime();
+  const comeDownDawn = burnTime + NINE_HR_SIX_M_MS;
+
+  if (time < comeDownDawn && time > burnTime) {
+    return true;
+  }
+
+  return false;
+};
+
+/**
+ * Figures out the Saturday before the next Labor Day,
+ * relative to now, or to dateOverride value.
+ *
+ * @param dateOverride Date. Optional. Explicit date (instead of now) for unit testing.
+ * @returns Date: when the Man will burn.
+ */
+export const getNextBurnTime = (dateOverride?: number) => {
+  const burnDate = dateOverride ? new Date(dateOverride) : new Date();
+  const currentYear = burnDate.getFullYear();
+  const calendarYearBurn = getBurnTime(currentYear);
+
+  if (burnDate < calendarYearBurn) return calendarYearBurn;
+
+  return getBurnTime(currentYear + 1);
+};
+
+export const getMetricTime = (nextBurnTime: number, referenceTime: number) => {
+  const xtianMsCount = nextBurnTime - referenceTime;
+
+  const metricSeconds = Math.floor(xtianMsCount / METRIC_SECOND_VALUE_MS);
+  const metricMinutes = Math.floor(metricSeconds / 100);
+  const metricHours = Math.floor(metricMinutes / 100);
+  const days = Math.floor(metricHours / 10).toString();
+
+  return {
+    year: getBurnYear(nextBurnTime).toString(),
+    days: addLeadingZeroes(days, 3),
+    hours: addLeadingZeroes((metricHours % 10).toString(), 2),
+    minutes: addLeadingZeroes((metricMinutes % 100).toString(), 2),
+    seconds: addLeadingZeroes((metricSeconds % 100).toString(), 2),
+  };
 };
